132210e6755b83d02be61bc28ef87ad996040c2d
Joanna Makarewicz <joanna.milun@gmail.com>
2022-10-13 17:08:12 +0200

    footer icons&frame for API data changes

--- src/js/index.js
+++ src/js/index.js
@@ -15,21 +15,19 @@ fetch("https://api.github.com/users/joannaMakarewicz/repos")
   
     const myTemplate = `        <article class="frame__main">
     <h3 class="frame__header">
-      <img class="frame__dots" src="../assets/icons/circles_top.png" />
+      <img class="frame__dots" src="../src/assets/icons/circles_top.png" />
     </h3>
     <div class="frame__content">
-      <img class="frame__icon" src="../assets/icons/GithubBlack.png" />
+      <img class="frame__icon" src="../src/assets/icons/GithubBlack.png" />
       <ul class="frame__list">
         <li class="frame__item">project:</li>
+        <li class="frame__project frame__list--2">${name}</li>
         <li class="frame__item frame__item--2">description:</li>
+        <li class="frame__description frame__list--2">${description}</li>
         <li class="frame__item">demo:</li>
+        <li class="frame__demo frame__list--2"><a href="">see here</a></li>
         <li class="frame__item">github:</li>
-      </ul>
-      <ul class="frame__list frame__list--2">
-      <li class="frame__project">${name}</li>
-      <li class="frame__description">${description}</li>
-      <li class="frame__demo"><a href="">see here</a></li>
-      <li class="frame__git"><a href="${html_url}">source code</a></li>
+        <li class="frame__git frame__list--2"><a href="${html_url}">source code</a></li>
       </ul>
     </div>
   </article>`;
